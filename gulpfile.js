2107b61c7ba1bf57cdd1856748678c4e5e83b341
Shylex96 <estebanalbarran1996@gmail.com>
2021-07-14 11:58:10 +0200

    FINALLY PROBLEM SOLVED.
    Needed to install node.js, npm install gulp and some more package until everything defined, compiled and solved.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Initialize modules
 const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -9,26 +9,26 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Use dart-sass for @use
-sass.compiler = require('dart-sass');
+//sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {
-	return src('app/scss/style.scss', {sourcemaps: true})
-	.pipe(sass())
-	.pipe(postcss([autoprefixer(), cssnano()]))
-	.pipe(dest('dist', {sourcemaps: '.'}));
+	return src('app/scss/style.scss', { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // JavaScript Task
 function jsTask() {
-	return src('app/js/script.js', {sourcemaps: true})
-	.pipe(babel({ presets: ['@babel/preset-env'] }))
-	.pipe(terser())
-	.pipe(dest('dist', {sourcemaps: '.' }));
+	return src('app/js/script.js', { sourcemaps: true })
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.pipe(terser())
+		.pipe(dest('dist', { sourcemaps: '.' }));
 }
 
 // Browsersync
-function broserSyncServe(cb) {
+function browserSyncServe(cb) {
 	browsersync.init({
 		server: {
 			baseDir: '.',
@@ -42,7 +42,7 @@ function broserSyncServe(cb) {
 	});
 	cb();
 }
-function broserSyncReLoad(cb) {
+function browserSyncReload(cb) {
 	browsersync.reload();
 	cb();
 }
